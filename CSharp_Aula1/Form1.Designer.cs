b4ce638f437b74bb8b062fbb1d4beb3daf66ff1f
Ezequiel <ezequielsennesantos@gmail.com>
2022-08-23 11:33:31 -0300

    Feito

--- CSharp_Aula1/Form1.Designer.cs
+++ CSharp_Aula1/Form1.Designer.cs
@@ -28,20 +28,147 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtSobrenome = new System.Windows.Forms.TextBox();
+            this.btnUniao = new System.Windows.Forms.Button();
+            this.txtNomeCompleto = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtN1 = new System.Windows.Forms.TextBox();
+            this.txtN2 = new System.Windows.Forms.TextBox();
+            this.txtN3 = new System.Windows.Forms.TextBox();
+            this.btnSoma = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(181, 12);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(268, 20);
+            this.txtNome.TabIndex = 0;
+            // 
+            // txtSobrenome
+            // 
+            this.txtSobrenome.Location = new System.Drawing.Point(181, 68);
+            this.txtSobrenome.Name = "txtSobrenome";
+            this.txtSobrenome.Size = new System.Drawing.Size(268, 20);
+            this.txtSobrenome.TabIndex = 0;
+            // 
+            // btnUniao
+            // 
+            this.btnUniao.Location = new System.Drawing.Point(181, 145);
+            this.btnUniao.Name = "btnUniao";
+            this.btnUniao.Size = new System.Drawing.Size(268, 54);
+            this.btnUniao.TabIndex = 1;
+            this.btnUniao.Text = "Unir nomes";
+            this.btnUniao.UseVisualStyleBackColor = true;
+            this.btnUniao.Click += new System.EventHandler(this.btnUniao_Click);
+            // 
+            // txtNomeCompleto
+            // 
+            this.txtNomeCompleto.Location = new System.Drawing.Point(181, 237);
+            this.txtNomeCompleto.Name = "txtNomeCompleto";
+            this.txtNomeCompleto.Size = new System.Drawing.Size(268, 20);
+            this.txtNomeCompleto.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(90, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(90, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Sobrenome";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(90, 240);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nome completo";
+            // 
+            // txtN1
+            // 
+            this.txtN1.Location = new System.Drawing.Point(640, 12);
+            this.txtN1.Multiline = true;
+            this.txtN1.Name = "txtN1";
+            this.txtN1.Size = new System.Drawing.Size(45, 20);
+            this.txtN1.TabIndex = 3;
+            // 
+            // txtN2
+            // 
+            this.txtN2.Location = new System.Drawing.Point(640, 68);
+            this.txtN2.Multiline = true;
+            this.txtN2.Name = "txtN2";
+            this.txtN2.Size = new System.Drawing.Size(45, 20);
+            this.txtN2.TabIndex = 3;
+            // 
+            // txtN3
+            // 
+            this.txtN3.Location = new System.Drawing.Point(562, 237);
+            this.txtN3.Multiline = true;
+            this.txtN3.Name = "txtN3";
+            this.txtN3.Size = new System.Drawing.Size(194, 20);
+            this.txtN3.TabIndex = 3;
+            // 
+            // btnSoma
+            // 
+            this.btnSoma.Location = new System.Drawing.Point(636, 142);
+            this.btnSoma.Name = "btnSoma";
+            this.btnSoma.Size = new System.Drawing.Size(48, 56);
+            this.btnSoma.TabIndex = 4;
+            this.btnSoma.Text = "Somar";
+            this.btnSoma.UseVisualStyleBackColor = true;
+            this.btnSoma.Click += new System.EventHandler(this.btnSoma_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSoma);
+            this.Controls.Add(this.txtN3);
+            this.Controls.Add(this.txtN2);
+            this.Controls.Add(this.txtN1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnUniao);
+            this.Controls.Add(this.txtNomeCompleto);
+            this.Controls.Add(this.txtSobrenome);
+            this.Controls.Add(this.txtNome);
             this.Name = "Form1";
             this.Text = " Ezequiel";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtSobrenome;
+        private System.Windows.Forms.Button btnUniao;
+        private System.Windows.Forms.TextBox txtNomeCompleto;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtN1;
+        private System.Windows.Forms.TextBox txtN2;
+        private System.Windows.Forms.TextBox txtN3;
+        private System.Windows.Forms.Button btnSoma;
     }
 }
 
--- CSharp_Aula1/Form1.cs
+++ CSharp_Aula1/Form1.cs
@@ -16,5 +16,40 @@ namespace CSharp_Aula1
         {
             InitializeComponent();
         }
+        string nome;
+        string sobrenome;
+        int N1;
+        int N2;
+
+        private void btnUniao_Click(object sender, EventArgs e)
+        {
+            nome = txtNome.Text;
+            sobrenome = txtSobrenome.Text;
+            nome = nome + " " + sobrenome;
+
+            txtNomeCompleto.Text = nome;
+        }
+
+        private void btnSoma_Click(object sender, EventArgs e)
+        {
+            N1 = int.Parse(txtN1.Text);
+            N2 = int.Parse(txtN2.Text);
+            N1 = N1 + N2;
+            if (N1>=10)
+            {
+                if (N1>10)
+                {
+                    txtN3.Text = "A soma é " + N1.ToString() + " e é maior que 10";
+                }
+                else
+                {
+                    txtN3.Text = "A soma é 10";
+                }
+            }
+            else
+            {
+                txtN3.Text = "A soma é " + N1.ToString() + " e é menor que 10";
+            }
+        }
     }
 }
