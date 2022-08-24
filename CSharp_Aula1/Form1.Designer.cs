20e28203339ed26140ed791391c1cedda75a91c2
Ezequiel <ezequielsennesantos@gmail.com>
2022-08-24 08:56:58 -0300

    Vamos

--- CSharp_Aula1/Form1.Designer.cs
+++ CSharp_Aula1/Form1.Designer.cs
@@ -39,25 +39,28 @@
             this.txtN2 = new System.Windows.Forms.TextBox();
             this.txtN3 = new System.Windows.Forms.TextBox();
             this.btnSoma = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txtNome
             // 
-            this.txtNome.Location = new System.Drawing.Point(181, 12);
+            this.txtNome.Location = new System.Drawing.Point(96, 8);
             this.txtNome.Name = "txtNome";
             this.txtNome.Size = new System.Drawing.Size(268, 20);
             this.txtNome.TabIndex = 0;
             // 
             // txtSobrenome
             // 
-            this.txtSobrenome.Location = new System.Drawing.Point(181, 68);
+            this.txtSobrenome.Location = new System.Drawing.Point(96, 64);
             this.txtSobrenome.Name = "txtSobrenome";
             this.txtSobrenome.Size = new System.Drawing.Size(268, 20);
             this.txtSobrenome.TabIndex = 0;
             // 
             // btnUniao
             // 
-            this.btnUniao.Location = new System.Drawing.Point(181, 145);
+            this.btnUniao.Location = new System.Drawing.Point(96, 141);
             this.btnUniao.Name = "btnUniao";
             this.btnUniao.Size = new System.Drawing.Size(268, 54);
             this.btnUniao.TabIndex = 1;
@@ -67,7 +70,7 @@
             // 
             // txtNomeCompleto
             // 
-            this.txtNomeCompleto.Location = new System.Drawing.Point(181, 237);
+            this.txtNomeCompleto.Location = new System.Drawing.Point(96, 233);
             this.txtNomeCompleto.Name = "txtNomeCompleto";
             this.txtNomeCompleto.Size = new System.Drawing.Size(268, 20);
             this.txtNomeCompleto.TabIndex = 0;
@@ -75,7 +78,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(90, 15);
+            this.label1.Location = new System.Drawing.Point(5, 11);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 2;
@@ -84,7 +87,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(90, 71);
+            this.label2.Location = new System.Drawing.Point(5, 67);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(61, 13);
             this.label2.TabIndex = 2;
@@ -93,7 +96,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(90, 240);
+            this.label3.Location = new System.Drawing.Point(5, 236);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(81, 13);
             this.label3.TabIndex = 2;
@@ -101,7 +104,7 @@
             // 
             // txtN1
             // 
-            this.txtN1.Location = new System.Drawing.Point(640, 12);
+            this.txtN1.Location = new System.Drawing.Point(555, 8);
             this.txtN1.Multiline = true;
             this.txtN1.Name = "txtN1";
             this.txtN1.Size = new System.Drawing.Size(45, 20);
@@ -109,7 +112,7 @@
             // 
             // txtN2
             // 
-            this.txtN2.Location = new System.Drawing.Point(640, 68);
+            this.txtN2.Location = new System.Drawing.Point(555, 64);
             this.txtN2.Multiline = true;
             this.txtN2.Name = "txtN2";
             this.txtN2.Size = new System.Drawing.Size(45, 20);
@@ -117,7 +120,7 @@
             // 
             // txtN3
             // 
-            this.txtN3.Location = new System.Drawing.Point(562, 237);
+            this.txtN3.Location = new System.Drawing.Point(477, 233);
             this.txtN3.Multiline = true;
             this.txtN3.Name = "txtN3";
             this.txtN3.Size = new System.Drawing.Size(194, 20);
@@ -125,7 +128,7 @@
             // 
             // btnSoma
             // 
-            this.btnSoma.Location = new System.Drawing.Point(636, 142);
+            this.btnSoma.Location = new System.Drawing.Point(551, 138);
             this.btnSoma.Name = "btnSoma";
             this.btnSoma.Size = new System.Drawing.Size(48, 56);
             this.btnSoma.TabIndex = 4;
@@ -133,11 +136,41 @@
             this.btnSoma.UseVisualStyleBackColor = true;
             this.btnSoma.Click += new System.EventHandler(this.btnSoma_Click);
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(498, 11);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(21, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "N1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(498, 67);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(21, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "N2";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(407, 236);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Resultado";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.btnSoma);
             this.Controls.Add(this.txtN3);
             this.Controls.Add(this.txtN2);
@@ -169,6 +202,9 @@
         private System.Windows.Forms.TextBox txtN2;
         private System.Windows.Forms.TextBox txtN3;
         private System.Windows.Forms.Button btnSoma;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
